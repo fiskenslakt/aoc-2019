a258f5c81491f6f8a97f4edfb84e79ac0662a723
Derek <sjukledighet@gmail.com>
2019-12-06 02:12:07 -0500

    Refactor solution for day 5

--- p05.py
+++ p05.py
@@ -1,161 +1,156 @@
+from typing import List
+
 from aocd import data
 
 
-codes = [code for code in data.split(',')]
+class InvalidIntcodeOp(Exception):
+    """Unknown Intcode operation."""
 
-pointer = 0
-while True:
-    # if pointer == 314:
-    #     break
-    print(pointer, codes[pointer:pointer+4])
-    *params, lead, op = codes[pointer].zfill(5)
-    arg1, arg2, arg3 = map(int, codes[pointer+1:pointer+4])
-    if op == '3':
-        # if params[-1] == '0':
-        #     # codes[1] = int(codes[arg1])
-        #     codes[1] = 1
-        # else:
-        #     # assert params[-1] == '1'
-        #     # codes[1] = arg1
-        #     codes[1] = 1
 
-        # codes[arg1] = 1  # part 1
-        codes[arg1] = 5  # part 2
+class Code:
+    def __init__(self, code: str, is_op: bool = False):
+        self.value = int(code)
+        self.is_op = is_op
+        self.op = None
+        self.modes = None
 
-        pointer += 2
+        if is_op:
+            *modes, lead, op = f'{self.value:05}'
+            self.op = int(lead + op)
+            # modes are given from
+            # right to left
+            self.modes = modes[::-1]
 
-    elif op == '4':
-        if params[-1] == '0':
-            print('OUTPUT:',codes[arg1])
+    def __repr__(self):
+        if self.is_op:
+            return f'Code({self.value}, {self.is_op})'
         else:
-            print('DEBUG OUTPUT:',arg1)
-            assert params[-1] == '1'
-            print('OUTPUT:',arg1)
+            return f'Code({self.value})'
 
-        pointer += 2
+    def __add__(self, other):
+        return self.value + other.value
 
-    elif op == '1':
-        if params[-1] == '0':
-            a = int(codes[arg1])
+    def __mul__(self, other):
+        return self.value * other.value
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __bool__(self):
+        return bool(self.value)
+
+
+class IntcodeComputer:
+    def __init__(self, program: List[str]):
+        self.program = program
+        self.memory = program.copy()
+        self.pointer = 0
+        self.diagnostic_codes = []
+
+    def _get_op(self) -> Code:
+        return Code(self.memory[self.pointer], is_op=True)
+
+    def _get_args(self, op: Code) -> List[Code]:
+        args = []
+
+        # get input paramters
+        for param, mode in zip(self.memory[self.pointer+1:self.pointer+3], op.modes):
+            param = Code(param)
+            if mode == '0' and op.op != 3:  # positional mode
+                param = Code(self.memory[param.value])
+                args.append(param)
+            else:  # immediate mode
+                args.append(param)
+
+            if op.op in (3, 4):
+                args.append(None)
+                break
+
+        # get output parameter
+        args.append(Code(self.memory[self.pointer+3]))
+
+        return args
+
+    def reset(self):
+        """Reset memory and pointer to
+        initial program state.
+        """
+        self.memory = self.program.copy()
+        self.pointer = 0
+        self.diagnostic_codes = []
+
+    def add(self, left_operand: Code, right_operand: Code, output: Code):
+        self.memory[output.value] = left_operand + right_operand
+        self.pointer += 4
+
+    def multiply(self, left_operand: Code, right_operand: Code, output: Code):
+        self.memory[output.value] = left_operand * right_operand
+        self.pointer += 4
+
+    def jump_if_true(self, predicate: Code, new_pointer: Code):
+        if predicate:
+            self.pointer = new_pointer.value
         else:
-            assert params[-1] == '1'
-            a = arg1
+            self.pointer += 3
 
-        if params[-2] == '0':
-            b = int(codes[arg2])
+    def jump_if_false(self, predicate: Code, new_pointer: Code):
+        if not predicate:
+            self.pointer = new_pointer.value
         else:
-            assert params[-2] == '1'
-            b = arg2
+            self.pointer += 3
 
-        if params[-3] == '0':
-            # c = int(codes[arg3])
-            c = arg3
+    def less_than(self, left_operand: Code, right_operand: Code, output: Code):
+        if left_operand < right_operand:
+            self.memory[output.value] = 1
         else:
-            assert params[-3] == '1'
-            c = arg3
+            self.memory[output.value] = 0
 
-        # print(params, op, codes[pointer])
-        # print(arg1, codes[arg1], arg2, codes[arg2], arg3, codes[arg3])
-        # print('debug', codes[c])
-        codes[c] = str(a + b)
-        # print('debug', codes[c])
+        self.pointer += 4
 
-        pointer += 4
+    def equals(self, left_operand: Code, right_operand: Code, output: Code):
+        if left_operand == right_operand:
+            self.memory[output.value] = 1
+        else:
+            self.memory[output.value] = 0
 
-    elif op == '2':
-        if params[-1] == '0':
-            a = int(codes[arg1])
-        else:
-            assert params[-1] == '1'
-            a = arg1
+        self.pointer += 4
 
-        if params[-2] == '0':
-            b = int(codes[arg2])
-        else:
-            assert params[-2] == '1'
-            b = arg2
+    def execute(self, system_id: str):
+        while (code := self._get_op()).value != 99:
+            arg1, arg2, arg3 = self._get_args(code)
 
-        if params[-3] == '0':
-            # c = int(codes[arg3])
-            c = arg3
-        else:
-            assert params[-3] == '1'
-            c = arg3
+            if code.op == 1:
+                self.add(arg1, arg2, arg3)
+            elif code.op == 2:
+                self.multiply(arg1, arg2, arg3)
+            elif code.op == 3:
+                self.memory[arg1.value] = system_id
+                self.pointer += 2
+            elif code.op == 4:
+                self.diagnostic_codes.append(arg1)
+                self.pointer += 2
+            elif code.op == 5:
+                self.jump_if_true(arg1, arg2)
+            elif code.op == 6:
+                self.jump_if_false(arg1, arg2)
+            elif code.op == 7:
+                self.less_than(arg1, arg2, arg3)
+            elif code.op == 8:
+                self.equals(arg1, arg2, arg3)
+            else:
+                raise InvalidIntcodeOp(f'Invalid Intcode op "{code.op}"')
 
-        # print(codes[pointer:pointer+4])
-        # print(params, op)
-        codes[c] = str(a * b)
 
-        pointer += 4
-    elif op == '5':
-        if params[-1] == '0':
-            a = int(codes[arg1])
-        else:
-            assert params[-1] == '1'
-            a = arg1
+program = [code for code in data.split(',')]
+computer = IntcodeComputer(program)
+computer.execute('1')
+print('Diagnostic codes:', computer.diagnostic_codes)
+print('Part 1:', computer.diagnostic_codes[-1].value)
 
-        if params[-2] == '0':
-            b = int(codes[arg2])
-        else:
-            assert params[-2] == '1'
-            b = arg2
-        if a != 0:
-            pointer = b
-        else:
-            pointer += 3
-    elif op == '6':
-        if params[-1] == '0':
-            a = int(codes[arg1])
-        else:
-            assert params[-1] == '1'
-            a = arg1
-
-        if params[-2] == '0':
-            b = int(codes[arg2])
-        else:
-            assert params[-2] == '1'
-            b = arg2
-        if a == 0:
-            pointer = b
-        else:
-            pointer += 3
-    elif op == '7':
-        if params[-1] == '0':
-            a = int(codes[arg1])
-        else:
-            assert params[-1] == '1'
-            a = arg1
-
-        if params[-2] == '0':
-            b = int(codes[arg2])
-        else:
-            assert params[-2] == '1'
-            b = arg2
-
-        if a < b:
-            codes[arg3] = 1
-        else:
-            codes[arg3] = 0
-
-        pointer += 4
-    elif op == '8':
-        if params[-1] == '0':
-            a = int(codes[arg1])
-        else:
-            assert params[-1] == '1'
-            a = arg1
-
-        if params[-2] == '0':
-            b = int(codes[arg2])
-        else:
-            assert params[-2] == '1'
-            b = arg2
-
-        if a == b:
-            codes[arg3] = 1
-        else:
-            codes[arg3] = 0
-
-        pointer += 4
-    else:
-        raise ValueError(f'invalid op code {op}')
+computer.reset()
+computer.execute('5')
+print('Diagnostic codes:', computer.diagnostic_codes)
+print('Part 2:', computer.diagnostic_codes[-1].value)
