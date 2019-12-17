a33d6e49f7b77219d96eaec473886ef7c8e24338
Derek <sjukledighet@gmail.com>
2019-12-17 02:29:19 -0500

    [posterity] raw solution for day 17 (both parts)

--- p17.py
+++ p17.py
@@ -1,19 +1,23 @@
-from collections import defaultdict
-
 from aocd import data
 
 from intcode import Computer
 
 
 class ASCII(Computer):
-    pass
+    def execute(self, instructions):
+        while (intcode := self._get_op_code()) != 99:
+            args = self._get_args(intcode)
+            op = self._get_op(intcode.op)
+            if intcode.op == 3:
+                op(*args, next(instructions), modes=intcode.modes)
+            else:
+                op(*args, modes=intcode.modes)
 
 
 ascii = ASCII(data)
 ascii.execute(None)
-# print(ascii.output)
+
 lines = []
-from pprint import pprint
 line = []
 for i in ascii.output:
     if i != 10:
@@ -22,23 +26,37 @@ for i in ascii.output:
         lines.append(line.copy())
         line = []
 
-# d = defaultdict(int)
 s = 0
 for y, line in enumerate(lines[:-1]):
     for x, c in enumerate(line):
         if c == 35:
             if x == len(line)-1 or x == 0 or y == len(lines)-1 or y == 0:
                 continue
-            # print(x, y)
             if lines[y][x-1] == lines[y][x+1] == lines[y-1][x] == lines[y+1][x]:
                 s += x*y
-                print(s)
-                # pos = complex(x,y)
-                # d[pos] += 1
 
-# s = 0
-# for p, i in d.items():
-#     if i == 2:
-#         s += p.real * p.imag
+print('Part 1:', s)
 
-print(s)
+# for line in lines:
+#     for c in line:
+#         print(chr(c), end='')
+#     print()
+
+ascii = ASCII(data)
+ascii.memory[0] = 2
+# bot = ascii.execute(None)
+# initialize bot
+# breakpoint()
+# bot.send(None)
+
+# path written out by hand
+# R,10,L,8,R,10,R,4,L,6,L,6,R,10,R,10,L,8,R,10,R,4,L,6,R,12,R,12,R,10,L,6,L,6,R,10,L,6,R,12,R,12,R,10,R,10,L,8,R,10,R,4,L,6,L,6,R,10,R,10,L,8,R,10,R,4,L,6,R,12,R,12,R,10
+
+A = 'L,6,L,6,R,10'
+B = 'R,10,L,8,R,10,R,4'
+C = 'L,6,R,12,R,12,R,10'
+main = 'B,A,B,C,A,C,B,A,B,C'
+
+instructions = [ord(i) for i in f'{main}\n{A}\n{B}\n{C}\nn\n']
+ascii.execute(iter(instructions))
+print('Part 2:', ascii.output[-1])
