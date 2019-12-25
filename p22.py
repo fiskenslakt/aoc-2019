c84bae577733dc1c93f90ab0857a2decdb279f90
Derek <sjukledighet@gmail.com>
2019-12-24 22:08:39 -0500

    Add solution for day 22

--- p22.py
+++ p22.py
@@ -5,15 +5,22 @@ from collections import deque
 from aocd import data
 
 
-def new_stack(deck):
+DECK_SIZE_SMALL = 10_007
+DECK_SIZE_BIG = 119_315_717_514_047
+REPEATS = 101_741_582_076_661
+CARD_NO = 2019
+CARD_POS = 2020
+
+
+def deal_new_stack(deck):
     deck.reverse()
 
 
-def cut(deck, n):
+def deal_cut(deck, n):
     deck.rotate(n*-1)
 
 
-def increment(deck, n):
+def deal_increment(deck, n):
     new_deck = deck.copy()
 
     for i, card in enumerate(deck):
@@ -23,18 +30,31 @@ def increment(deck, n):
 
 
 p = re.compile(r'(\w+)\s(-?\d+)')
-deck = deque(range(10_007))
+deck = deque(range(DECK_SIZE_SMALL))
+increment_multiplier = 1
+offset_difference = 0
 
 for shuffle in data.splitlines():
     if 'new' in shuffle:
-        new_stack(deck)
+        deal_new_stack(deck)
+        increment_multiplier = (increment_multiplier * -1) % DECK_SIZE_BIG
+        offset_difference = (increment_multiplier + offset_difference) % DECK_SIZE_BIG
     else:
         shuffle_type, n = p.search(shuffle).groups()
         n = int(n)
 
         if shuffle_type == 'cut':
-            cut(deck, n)
+            deal_cut(deck, n)
+            offset_difference = (offset_difference + (n * increment_multiplier)) % DECK_SIZE_BIG
         elif shuffle_type == 'increment':
-            deck = increment(deck, n)
+            deck = deal_increment(deck, n)
+            increment_multiplier = (increment_multiplier * pow(n, DECK_SIZE_BIG-2, DECK_SIZE_BIG)) % DECK_SIZE_BIG
 
-print('Part 1:', deck.index(2019))
+print('Part 1:', deck.index(CARD_NO))
+
+increment = pow(increment_multiplier, REPEATS, DECK_SIZE_BIG)
+offset = offset_difference * (1 - increment) * pow((1 - increment_multiplier) % DECK_SIZE_BIG, DECK_SIZE_BIG-2, DECK_SIZE_BIG)
+offset %= DECK_SIZE_BIG
+
+card = (offset + CARD_POS * increment) % DECK_SIZE_BIG
+print('Part 2:', card)
